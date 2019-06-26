abb4c9b7a890cd41ca5a70fee56f5b087e3f32cf
Yu Jin Kim <yujinkim@fb.com>
2019-06-26 11:03:58 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by yujinkim on 6/26/19.
+//  Copyright © 2019 yujinkim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flix
+//
+//  Created by yujinkim on 6/26/19.
+//  Copyright © 2019 yujinkim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
