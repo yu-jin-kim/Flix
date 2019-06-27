460b704660b8e1cdd97a9b562bea292bc9a46c61
Yu Jin Kim <yujinkim@fb.com>
2019-06-26 17:51:12 -0700

    required stories

---
+++ DetailsViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailsViewController.h
+//  
+//
+//  Created by yujinkim on 6/26/19.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
